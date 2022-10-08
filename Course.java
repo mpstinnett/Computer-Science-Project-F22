44ac7e4d986a8ffafe071513e6d75dfc32346665
micheal stinet <mpstinnett@gmail.com>
2022-10-08 18:14:59 -0500

    fixed .class to .java

---
+++ Course.java
@@ -0,0 +1,10 @@
+public class Course {
+    String courseNum;
+    String semester;
+    Double grade;
+    String courseTitle;
+    Boolean transfer;
+
+    Course () {};
+
+}
---
+++ Student.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Student {
+    String name;
+    String ID;
+    String semAdmitted;
+    ArrayList<Course> Courses = new ArrayList<Course>();
+    Student () {};
+}
