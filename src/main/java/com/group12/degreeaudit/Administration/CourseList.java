7b994ee90b92d273d16e74403118ba7da811b296
LoadUpStudios <31973893+BrandonT80@users.noreply.github.com>
2022-10-08 19:02:17 -0500

    Create Maven Project

---
+++ src/main/java/com/group12/degreeaudit/Administration/CourseList.java
@@ -0,0 +1,20 @@
+package com.group12.degreeaudit.Administration;
+
+public class CourseList
+{
+
+    public boolean CreateList()
+    {
+        return false;
+    }
+
+    public boolean AddCourse()
+    {
+        return false;
+    }
+
+    public boolean RemoveCourse()
+    {
+        return false;
+    }
+}
